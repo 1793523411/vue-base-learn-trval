94678523863acce13855d859883422c1e3ffa650
ygjzs <1793523411@qq.com>
2020-07-14 18:28:28 +0800

    header

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: "Home",
+  components:{
+    HomeHeader
+  }
+};
+</script>
+
+<style>
+.sty {
+  font-size: 50px;
+}
+</style>
