4c3de1bebd2b493934001e81de5e99527b5f47e8
ygjzs <1793523411@qq.com>
2020-07-14 22:41:11 +0800

    change

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# vuecli
+# taval
+
++ 引入一些基本的css文件（一般基本的，处理移动端的，iconfont）
++ 引入iconfont并使用:(https://www.iconfont.cn)
++ 使用stylus，并设置全局变量（例如颜色，高度等）安装`stylus`和`stylus-loader`
++ 引入fastclick处理移动端点击事件，安装`fastclick`
++ 先把header做出来
++ 轮播图 `vue-awesome-swiper@2.6.7` （https://github.com/surmon-china/vue-awesome-swiper）
 
 > A Vue.js project
 
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwpier",
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        loop: true,
+        autoplay: 1000,
+        // Some Swiper option/callback...
+      },
+      list:[{
+        imgUrl: 'https://img-blog.csdnimg.cn/20200208181017569.gif'
+      },{
+        imgUrl: 'https://img-blog.csdnimg.cn/20200208181013160.gif'
+      }]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
