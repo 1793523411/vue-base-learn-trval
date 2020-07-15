5ec737e6ae31b169eaa93f727073489a83d4cd95
ygjzs <1793523411@qq.com>
2020-07-15 23:33:45 +0800

    search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,17 +1,21 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
-import CityHeader from './components/Header'
+import CityHeader from "./components/Header";
+import CitySearch from "./components/Search";
 export default {
-  name:'City',
+  name: "City",
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
-}
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+     <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch',
+  data() {
+    return {
+      keyword: ''
+    }
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .search-input
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      padding: 0 .1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
