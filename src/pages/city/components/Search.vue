c66e7a2fe15f92e9d66ce93b237ac735d4684bbc
ygjzs <1793523411@qq.com>
2020-07-19 20:27:00 +0800

    finish-search

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -3,17 +3,64 @@
      <div class="search">
       <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到数据匹配项</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
   name: 'CitySearch',
+  props: {
+    cities: Object
+  },
   data() {
     return {
-      keyword: ''
+      keyword: '',
+      list: [],
+      timer: null
     }
   },
+  computed: {
+    hasNoData(){
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword(){
+      if(this.timer){
+        clearTimeout(this.timer)
+      }
+      if(!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+          const result = []
+          for(let i in this.cities){
+            this.cities[i].forEach((value) => {
+              if(value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1){
+                result.push(value)
+              }
+            })
+          }
+          this.list = result
+      }, 100);
+    }
+  },
+  methods: {
+
+  },
+  mounted(){
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -31,5 +78,19 @@ export default {
       line-height: .62rem
       text-align: center
       border-radius: .06rem
+      color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      background: #fff
       color: #666
 </style>
